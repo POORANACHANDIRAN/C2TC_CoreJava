3805857e5ba410ec684464dd021ac89b2adc2382
91637 <91637@LAPTOP-K2G5JQM3>
2023-07-28 15:53:57 +0530

    Pooranachandiran G

---
+++ sample/src/com/cg/dsytwo/condition/Condition.java
@@ -0,0 +1,15 @@
+package com.cg.dsytwo.condition;
+
+public class Condition {
+	public static void main(String[]args) {
+		int a=10;
+		if(a%2==0) {
+			System.out.println("the given number is even");
+		}
+		else {
+			System.out.println("the given number is odd");
+		}
+	}
+	
+
+}
